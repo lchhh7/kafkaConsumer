df79ba00c50165b42e3b5a933f1e22619891746e
chlee <changhoon1994@gmail.com>
2023-07-27 17:22:37 +0900

    Initial commit

---
+++ src/main/java/com/kafka/kafkatest/KafkaTestApplication.java
@@ -0,0 +1,16 @@
+package com.kafka.kafkatest;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+
+@SpringBootApplication
+public class KafkaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTestApplication.class, args);
+    }
+}
