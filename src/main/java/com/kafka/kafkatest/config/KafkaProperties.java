52b8fc121a3fca6ad843dcfc4fafec918d04ea9c
chlee <changhoon1994@gmail.com>
2023-08-01 15:51:24 +0900

    remote test

--- src/main/java/com/kafka/kafkatest/config/KafkaProperties.java
+++ src/main/java/com/kafka/kafkatest/config/KafkaProperties.java
@@ -1,9 +1,7 @@
 package com.kafka.kafkatest.config;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("spring.kafka")
