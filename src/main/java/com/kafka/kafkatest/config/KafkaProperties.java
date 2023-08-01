0deb6b3ea83c0b826ba14de9d49f37a78111f7bf
chlee <changhoon1994@gmail.com>
2023-08-01 15:07:09 +0900

    yml setting

--- src/main/java/com/kafka/kafkatest/config/KafkaProperties.java
+++ src/main/java/com/kafka/kafkatest/config/KafkaProperties.java
@@ -6,13 +6,11 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("kafka")
+@ConfigurationProperties("spring.kafka")
 @Getter
 @Setter
 public class KafkaProperties {
     public static final String CONSUMER_GROUP_ID = "testGroup";
 
-
-    @Value("localhost:9092")
     private String bootStrapServers;
 }
